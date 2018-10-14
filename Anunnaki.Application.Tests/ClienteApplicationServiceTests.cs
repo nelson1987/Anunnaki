c0ab20a79bee055802bdcd2b67b7925cd0010520
Nelson Neto <nelson.ash@outlook.com>
2018-10-14 08:35:54 -0300

    ApplicationService Tests

---
+++ Anunnaki.Application.Tests/ClienteApplicationServiceTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Anunnaki.Application.Tests
+{
+    //[TestCase]
+    public class ClienteApplicationServiceTests
+    {
+        [Test]
+        private void CadastrarCliente()
+        {
+            var nomeCliente = "";
+            var emailCliente = "";
+
+            var cliente = new Cliente();
+            cliente.Nome = nomeCliente;
+            cliente.Email = emailCliente;
+            cliente.isValid();
+            Assert.AreEqual(true, cliente.isValid());
+        }
+    }
+}
